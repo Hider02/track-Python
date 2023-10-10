01a0fb73afcd4de83d9a7a763e71e0620be5f8cc
Hider02 <147307487+Hider02@users.noreply.github.com>
2023-10-10 10:30:35 +0300

    Add files via upload

---
+++ лабораторная работа 1/task3.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+amount_players = len(list_players)
+first_team = list_players[:(amount_players//2)]
+second_team = list_players[(amount_players//2):]
+print(first_team)
+print(second_team)
+# TODO Разделите участников на две команды
