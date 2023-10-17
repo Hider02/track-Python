d59a5e119d0669ecfd6c572ab1f3499e06db8db1
Hider02 <147307487+Hider02@users.noreply.github.com>
2023-10-17 14:06:45 +0300

    Update task3.py

--- лабораторная работа 1/task3.py
+++ лабораторная работа 1/task3.py
@@ -1,7 +1,8 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 amount_players = len(list_players)
-first_team = list_players[:(amount_players//2)]
-second_team = list_players[(amount_players//2):]
+HALF = amount_players//2
+first_team = list_players[:HALF]
+second_team = list_players[HALF:]
 print(first_team)
 print(second_team)
 # TODO Разделите участников на две команды
